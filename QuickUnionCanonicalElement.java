2d79d9c59146e611532ce39e36622aad89d137e7
Alejandro <alejandro.monasterios@teamsoftware.com>
2019-07-01 18:43:08 -0400

    add docs to canonical example

--- QuickUnionCanonicalElement.java
+++ QuickUnionCanonicalElement.java
@@ -1,7 +1,12 @@
 import java.util.Arrays;
 
 /**
- * Quick Union Data
+ * Quick Union 
+ * Union-find with specific canonical element. Add a method find() to the union-find data type so 
+ * that find(i) returns the largest element in the connected component containing iii. 
+ * The operations, union(), connected(), and find() should all take logarithmic time or better.
+ * For example, if one of the connected components is {1,2,6,9}, then the find() method 
+ * should return 999 for each of the four elements in the connected components.
  * 
  */
 public class QuickUnionCanonicalElement {

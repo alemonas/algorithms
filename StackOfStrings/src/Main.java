49799d475af03d66adbf08f6b25ce47f93c2e8c3
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2019-09-20 18:26:34 -0400

    Stack linked List and array resize:

---
+++ StackOfStrings/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        String a = "my string";
+        stack.push(a);
+        a = "my new string";
+        stack.push(a);
+        String res = stack.pop();
+
+        System.out.println(res);
+    }
+}

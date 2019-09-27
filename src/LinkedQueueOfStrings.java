87399b956b3c873828d1231eb24ae82f2619876a
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2019-09-27 08:36:42 -0400

    WIP Linked List Stack Queue:

---
+++ src/LinkedQueueOfStrings.java
@@ -0,0 +1,20 @@
+public class LinkedQueueOfStrings {
+    private Node first, last;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first ==  null;
+    }
+
+    public void enqueue(String item) {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+
+
+    }
+}
